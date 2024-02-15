d78ac54e1978aee8b88d1355eafa697b1555bd15
raphael <raphael.birkl@gmail.com>
2024-02-15 16:46:30 +0100

    task 3 done

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:streams_exercises/features/chat/chat_message.dart';
 import 'package:streams_exercises/features/chat/chat_repository.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  final ChatRepository chatRepository;
+
   const ChatScreen({
     super.key,
     required this.chatRepository,
   });
 
-  final ChatRepository chatRepository;
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    widget.chatRepository.startSendingMessages();
+  }
+
+  @override
+  void dispose() {
+    widget.chatRepository.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +33,28 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Chat Screen'),
       ),
-      body: const Center(
-        child: Text("Hier sollen die Chatnachrichten stehen"),
+      body: Center(
+        child: Column(
+          children: [
+            OutlinedButton(
+                onPressed: () {
+                  widget.chatRepository.startSendingMessages();
+                },
+                child: const Text("start the chat")),
+            StreamBuilder<ChatMessage>(
+                stream: widget.chatRepository.messages,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final message = snapshot.data;
+                    return Text('${message?.user}: ${message?.message}');
+                  } else if (snapshot.hasError) {
+                    return Text("Error ${snapshot.error}");
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                }),
+          ],
+        ),
       ),
     );
   }
